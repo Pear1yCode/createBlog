5e9e8875c28d50eee1d3bcb6d942751e3d7934c0
이호준 <gh_wns@naver.com>
2024-08-29 17:51:02 +0900

    no message

---
+++ main, post, game/snow.js
@@ -0,0 +1,23 @@
+for (let i = 0; i < 250; i++) {
+  const snow = document.createElement("div");
+  snow.className = "snow";
+  snow.style.opacity = Math.random();
+
+  const startX = Math.random() * 93; // 눈 범위 width
+  const endX = startX + (Math.random() * 20 - 10);
+  const scale = Math.max(Math.random(), 0.5);
+
+  const keyframe = [
+    { transform: `translate(${startX}vw, 0) scale(${scale})` },
+    { transform: `translate(${endX}vw, 175vh) scale(${scale})` }, // 눈 범위 height
+  ];
+  const option = {
+    duration: 15000,
+    easing: "linear",
+    iterations: Infinity,
+    delay: -Math.random() * 20 * 1000,
+  };
+  snow.animate(keyframe, option);
+
+  document.body.appendChild(snow);
+}
